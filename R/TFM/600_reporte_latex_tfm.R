a856415fd0ebf05c8964b818bfc54b9ff0084636
Cristian_Guatemal <cristian.guatemal@gmail.com>
2024-06-20 07:09:45 -0500

    Correcciones

--- R/TFM/600_reporte_latex_tfm.R
+++ R/TFM/600_reporte_latex_tfm.R
@@ -1,5 +1,5 @@
 # Preparación
-source( 'R/400_preparar_reporte.R', encoding = 'UTF-8', echo = FALSE )
+source( 'R/400_preparar_proyecto.R', encoding = 'UTF-8', echo = FALSE )
 
 # Reporte LaTeX
 source( parametros$reporte_script, encoding = 'UTF-8', echo = FALSE )
